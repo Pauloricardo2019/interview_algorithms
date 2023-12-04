a9015cfdd7ccfb289e4702f15b2f3a727d3295a4
Pauloricardo2019 <paullin_santos13@hotmail.com>
2023-12-04 16:09:27 -0300

    ::

--- medium/subarray_with_given_sum.go
+++ medium/subarray_with_given_sum.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 /*
+LINK: https://practice.geeksforgeeks.org/problems/subarray-with-given-sum-1587115621/1
+
+
 Description:
 Given an unsorted array A of size N that contains only positive integers, find a continuous sub-array that adds to a given number S and return the left and right index(1-based indexing) of that subarray.
 
